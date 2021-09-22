c41920f1bc086ea3c67abd2e68a29c73f3cab709
Zathras <ChampionMike42@gmail.com>
2021-09-22 14:21:32 -0500

    Add Project.jsx

---
+++ src/components/Projects.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+
+function Projects() {
+  
+
+  return (
+    <div id="about" className="flex flex-col items-center my-4">This is the Projects section
+    <br />
+      <img alt="blank" className="h-60 w-60 bg-blue-600"></img>
+    </div>
+  )
+}
+
+export default Projects
