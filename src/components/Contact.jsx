271b417769cf764d7eebd24cd3dd2da078b5e5f1
Zathras <ChampionMike42@gmail.com>
2021-09-22 14:21:23 -0500

    Add Contact.jsx

---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+
+function Contact() {
+  
+
+  return (
+    <div id="about" className="flex flex-col items-center my-4">This is the Contact section
+    <br />
+      <img alt="blank" className="h-60 w-60 bg-red-600"></img>
+    </div>
+  )
+}
+
+export default Contact
