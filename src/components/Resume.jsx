33b51f1c192d2084f800ee0637719d82debffc97
Zathras <ChampionMike42@gmail.com>
2021-09-22 14:21:40 -0500

    Add Resume.jsx

---
+++ src/components/Resume.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+
+function Resume() {
+  
+
+  return (
+    <div id="about" className="flex flex-col items-center my-4">This is the Resume section
+    <br />
+      <img alt="blank" className="h-60 w-60 bg-yellow-600"></img>
+    </div>
+  )
+}
+
+export default Resume
