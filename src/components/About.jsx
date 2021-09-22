3e9550049245da8eff257db1df3e18f722d07591
Zathras <ChampionMike42@gmail.com>
2021-09-22 14:21:03 -0500

    Add About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+
+function About() {
+  
+
+  return (
+    <div id="about" className="flex flex-col items-center my-4">This is the About section
+    <br />
+      <img alt="blank" className="h-60 w-60 bg-green-600"></img>
+    </div>
+  )
+}
+
+export default About
