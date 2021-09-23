e4aff889c97fb46566143bf1e1c28de686b38358
Zathras <ChampionMike42@gmail.com>
2021-09-23 12:00:16 -0500

    Remove unused import

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, { useState } from "react";
+import React from "react";
 // import { render } from "react-dom";
 // import { NavLinks } from "./NavLinks";
 
