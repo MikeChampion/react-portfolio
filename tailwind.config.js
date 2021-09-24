dc96d629bd16dc9e2c8d7d2fd99bb7e78539f9cd
Zathras <ChampionMike42@gmail.com>
2021-09-23 19:26:15 -0500

    Add ref for bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+        backgroundImage: {
+            'generator': "url('./images/generator.jpg')",
+           }
+    },
   },
   variants: {
     extend: {},
