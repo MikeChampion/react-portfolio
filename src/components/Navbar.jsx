8f9726a7944f4e6375587b1e63e34c3c7c6dc4b0
Zathras <ChampionMike42@gmail.com>
2021-09-26 00:50:34 -0500

    Update Footer and Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,24 +22,23 @@ const navLinks = [
 ];
 
 function Navbar(props) {
-  return (
-    <nav className="flex justify-between items-center w-full bg-blue-700 text-gray-100 p-4">
-      <h1 className="md:text-2xl">Mike Champion</h1>
-      <div className="flex flex-row justify-end list-none md:gap-2">
-        {navLinks.map((link, index) => (
-            <NavLink key={index} to={link.route} activeClassName='bg-gray-300 text-blue-700' className="font-sm px-2 py-1 underline rounded-lg hover:text-black transition">
-                {link.name}
-            </NavLink>
-          ))}
-      </div>
-      
-    </nav>
-  )
+    return (
+        <nav className="flex flex-col md:flex-row justify-between gap-2 items-center w-full bg-blue-700 text-gray-100 p-4">
+            <h1 className="text-xl md:text-2xl lg:text-3xl">Mike Champion</h1>
+            <div id="nav" className="flex flex-row  flex-wrap md:flex-nowrap gap-1 md:gap-2">
+                {navLinks.map((link, index) => (
+                    <NavLink key={index} to={link.route} activeClassName='bg-gray-300 text-blue-700' className="text-sm md:text-lg px-2 py-1 underline rounded-lg hover:text-black transition">
+                        {link.name}
+                    </NavLink>
+                ))}
+            </div>
+        </nav>
+    )
 }
   
 Navbar.propTypes = {
-  name: PropTypes.string,
-  route: PropTypes.string,
+    name: PropTypes.string,
+    route: PropTypes.string,
 };
 
 export default Navbar
