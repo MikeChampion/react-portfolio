6c265e93a0d333cf6605f2807f2303d873b3d9f2
Zathras <ChampionMike42@gmail.com>
2021-09-27 15:35:50 -0500

    Update footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,31 +8,27 @@ function Footer(props) {
     {
         name: "GitHub",
         url: "https://github.com/MikeChampion",
-        icon: "github",
+        icon: "logo-github",
     },
     {
         name: "LinkedIn",
         url: "https://www.linkedin.com/in/MikeChampion42/",
-        icon: "linkedin",
+        icon: "logo-linkedin",
     },
     {
         name: "Stack Overflow",
-        url: "https://www.facebook.com/Rassilon007",
-        icon: "facebook",
+        url: "https://stackoverflow.com/users/8723496/mike-champion",
+        icon: "logo-stackoverflow",
     },
-    {
-      name: "Reddit",
-      url: "https://www.reddit.com/user/Rassilon007",
-      icon: "reddit",
-  },
+    
   ];
 
   return (
-    <footer className="flex flex-row space-between justify-center items-center w-full gap-4 bg-blue-700 text-gray-100 py-1 fixed bottom-0">
+    <footer className="flex flex-row space-between justify-center items-center w-full gap-4 bg-blue-700 text-gray-100 fixed bottom-0">
       <div className="flex justify-center w-10 list-none gap-4">
         {footerLinks.map((link, index) => (
             <button key={index}>
-              <a href={link.url} target="_blank" rel="noreferrer"><i title={link.name} className={`ion-logo-${link.icon} text-2xl`}></i></a>
+              <a href={link.url} target="_blank" rel="noreferrer"><ion-icon title={link.name} name={link.icon} class={"text-2xl pt-3 pb-2 text-gray-100"}></ion-icon></a>
             </button>
           ))}
       </div>
