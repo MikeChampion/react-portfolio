cca7111d67dcd3e8653224c4849d892e03edd36d
Zathras <ChampionMike42@gmail.com>
2021-09-27 21:09:02 -0500

    Update project tiles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,67 +1,88 @@
 import PropTypes from "prop-types";
 
 function Projects() {
+    {/* Images should be 750w x 400h x 72dpi */}
     const projects = [
         {
-            name: "Password Generator",
-            githubURL: "https://github.com/MikeChampion/Password_Generator",
-            githubicon: "github",
-            deployURL: "https://mikechampion.github.io/Password_Generator/",
-            deployicon: "pulse",
-            stack: "HTML, CSS, JS",
-            image: "generator",
+            name: "Fitness Tracker",
+            githubURL: "https://github.com/MikeChampion/fitness-tracker",
+            icon: "logo-github",
+            deployURL: "https://frozen-dusk-31622.herokuapp.com/",
+            stack: "HTML, CSS, NodeJS, Express, Morgan, MySQL",
+            image: "fitness.jpg"
+        },
+        {
+            name: "Tech Blog",
+            githubURL: "https://github.com/MikeChampion/tech-blog",
+            icon: "logo-github",
+            deployURL: "https://fierce-scrubland-84529.herokuapp.com/",
+            stack: "HTML, CSS, Handlebars, NodeJS, Express, MySQL",
+            image: "techBlog.jpg"
+        },
+        {
+            name: "Employee Tracker",
+            githubURL: "https://github.com/MikeChampion/employee-tracker",
+            icon: "play-circle-outline",
+            deployURL: "https://watch.screencastify.com/v/jyvtj5KVKoF1fjMzUNSB",
+            stack: "NodeJS, MySQL",
+            image: "empTracker.png"
+        },
+        {
+            name: "Team Builder",
+            githubURL: "https://github.com/MikeChampion/team-builder",
+            icon: "play-circle-outline",
+            deployURL: "https://watch.screencastify.com/v/woGBJdJ9EAjG5RZ8fgFL",
+            stack: "HTML, CSS, NodeJS, Inquirer, Jest",
+            image: "teamBuilder.png"
+        },
+        {
+            name: "Weather App",
+            githubURL: "https://github.com/MikeChampion/weather-app",
+            icon: "logo-github",
+            deployURL: "https://mikechampion.github.io/weather-app/",
+            stack: "HTML, CSS, JS, OpenWeatherMap API",
+            image: "weather.jpg"
         },
         {
             name: "Password Generator",
             githubURL: "https://github.com/MikeChampion/Password_Generator",
-            githubicon: "github",
+            icon: "logo-github",
             deployURL: "https://mikechampion.github.io/Password_Generator/",
-            deployicon: "pulse",
             stack: "HTML, CSS, JS",
-            image: "generator",
-        },
-        {
-            name: "Password Generator",
-            githubURL: "https://github.com/MikeChampion/Password_Generator",
-            githubicon: "github",
-            deployURL: "https://mikechampion.github.io/Password_Generator/",
-            deployicon: "pulse",
-            stack: "HTML, CSS, JS",
-            image: "generator",
+            image: "generator.jpg"
         },
       ];
        
     return (
-        <div id="projectsContainer" className="flex flex-col md:flex-row md:flex-wrap items-center md:justify-evenly m-8 w-11/12 lg:w-5/6 gap-4">
-            <h2>My projects</h2>
-            {/* Images should be 750w x 400h x 72dpi */}
+        <div className="flex flex-col items-center mt-8 w-11/12">
+        <h2 className="text-3xl font-bold">My projects</h2>
+        <div id="projectsContainer" className="flex flex-col md:flex-row md:flex-wrap items-center md:justify-evenly mt-8 mb-20 w-11/12 lg:w-5/6 gap-4">
                 {projects.map((project, index) => (
-                    <div key={index} className="projectTile relative flex flex-wrap border-1 border-blue-700 rounded-lg w-11/12 md:w-5/12 lg:w-1/4">
-                        <img src="/images/generator.jpg" alt="" className="rounded-lg w-full" />
-                        <div className="projectOverlay flex flex-col gap-2 m-4 py-4 px-6 border-2 border-blue-900 lg:border-0 lg:m-0 lg:py-4 lg:px-6 lg:w-full lg:h-full lg:pl-10 lg:pt-12 bg-gray-200 bg-opacity-60 rounded-lg text-blue-900 absolute lg:opacity-0 lg:hover:opacity-100 lg:transition lg:delay-150">
-                            <div className="projectTitle flex flex-row items-end gap-2 lg:gap-4 font-bold">
-                                <a href={project.deployURL} className="underline md:text-md lg:text-lg" target="_blank" rel="noreferrer">
+                    <div key={index} className="projectTile relative flex flex-wrap border border-blue-700 rounded-lg w-11/12 md:w-5/12 lg:w-1/4">
+                        <img src={`./images/${project.image}`} alt="" className="rounded-lg w-full" />
+                        <div className="projectOverlay flex flex-col gap-2 m-4 md:m-2 py-4 px-6 border-2 border-blue-900 lg:border-0 lg:m-0 lg:py-4 lg:px-6 lg:w-full lg:h-full lg:pl-10 lg:pt-12 bg-gray-100 bg-opacity-60 rounded-lg text-blue-900 absolute lg:opacity-0 lg:hover:opacity-100 lg:transition lg:delay-150">
+                            <div className="projectTitle flex flex-row justify-start items-center gap-2 lg:gap-4 font-bold">
+                                <a href={project.deployURL} className="underline text-base md:text-lg lg:text-xl" target="_blank" rel="noreferrer">
                                     {project.name}
                                 </a>
                                 <a href={project.githubURL} className="md:self-start" target="_blank" rel="noreferrer">
-                                    <i title={project.name} className={`ion-logo-${project.githubicon} text-2xl`}></i>
+                                    <ion-icon title={project.name} name={project.icon} class={"text-lg md:text-xl lg:text-2xl text-blue-900"}></ion-icon>
                                 </a>
                             </div>
-                            <div className="font-bold">{project.stack}</div>  
+                            <div className="text-xs md:text-sm lg:text-base font-bold">{project.stack}</div>  
                         </div>
                     </div>
                 ))}
             
-        </div>    
+        </div>    </div>
     )
 }
 
 Projects.propTypes = {
     name: PropTypes.string,
     githubURL: PropTypes.string,
-    githubicon: PropTypes.string,
+    icon: PropTypes.string,
     deployURL: PropTypes.string,
-    deployicon: PropTypes.string,
     stack: PropTypes.string,
     image: PropTypes.string
   };
