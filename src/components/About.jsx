c3905a15cf976f3d38883c993ecd9764e0d20888
Zathras <ChampionMike42@gmail.com>
2021-09-29 11:44:41 -0500

    Minor layout tweaks

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,13 +5,13 @@ function About() {
         <div className="flex flex-col items-center mt-8 w-11/12">
             <h2 className="text-3xl font-bold">About Me</h2>
             <div className="flex flex-col items-center mt-8 mb-20 w-11/12 md:w-3/4 lg:w-1/2 gap-4">
-                <div className="flex flex-col md:flex-row md:items-end items-center gap-4">
+                <div className="flex flex-col md:flex-row md:justify-around md:items-center items-center gap-4 md:gap-2">
                     <img
                         src={headshot}
                         alt=""
                         className="w-3/4 md:w-1/3 rounded-full"
                     />
-                    <p className="">
+                    <p className="md:w-3/5 lg:w-2/5">
                         Hi, I'm Mike Champion, and I'm here to
                         'champion' any web project you have to the
                         finish line! In my free time I play tabletop board
